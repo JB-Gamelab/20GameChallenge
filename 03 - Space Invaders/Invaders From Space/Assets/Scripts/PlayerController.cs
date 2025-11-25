7333b611df827af5869023577b831fa5660fd015
JB-Gamelab <132678018+JB-Gamelab@users.noreply.github.com>
2025-11-25 22:36:47 +0000

    Space Invaders
    Setup player movement including x axis limits

--- 03 - Space Invaders/Invaders From Space/Assets/Scripts/PlayerController.cs
+++ 03 - Space Invaders/Invaders From Space/Assets/Scripts/PlayerController.cs
@@ -4,6 +4,8 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float speed = 5f;
+    [SerializeField] private float maxXPos = 8f;
+    [SerializeField] private float yPos = -3.6f;
     private Vector2 move;
 
     private void OnMove(InputValue val)
@@ -13,6 +15,17 @@ public class PlayerController : MonoBehaviour
 
     private void Update()
     {
-        transform.position += speed * Time.deltaTime * new Vector3(move.x, -3.6f, 0f);
+        if (transform.position.x >= maxXPos * -1 && transform.position.x <= maxXPos)
+        {
+            transform.position += speed * Time.deltaTime * new Vector3(move.x, 0f, 0f);
+        }
+        if (transform.position.x < maxXPos * -1)
+        {
+            transform.position = new Vector2(maxXPos * -1, yPos);
+        }
+        if (transform.position.x > maxXPos)
+        {
+            transform.position = new Vector2(maxXPos, yPos);
+        }
     }
 }
